f322411f0be5485d74a20bde5523432c1ea51ae0
tatogotsiridze-cell <tatogotsiridze@gmail.com>
2026-01-21 20:17:44 +0400

    davaleba

---
+++ script.js
@@ -0,0 +1,11 @@
+const firstName = prompt("What's your first name?");
+const lastName = prompt("What's your last name?");
+const age = prompt("How old are you?");
+const username = prompt("What's your username?");
+const password = prompt("What's your password?");
+
+alert("First Name: " + firstName +
+      "Last Name: " + lastName +
+      "Age: " + age + " years old" +
+      "Username: " + username +
+      "Password: " + password);
