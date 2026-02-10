eab850134380b20103a144f17529909a9131bd79
tatogotsiridze-cell <tatogotsiridze@gmail.com>
2026-02-10 18:35:41 +0400

    asasd

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const age = prompt ("Please enter your age:");
-const hastiket = prompt ("do you have ticket :");
-const p=prompt ("are you  whit parent :");
-const lt = prompt("are you late :");
-if (age >= 18 && hastiket === "yes" && lt === "no") {
-    alert("  you can enter ");
-} else if (age < 13 && p === "yes" && hastiket === "yes" ) {
-    alert("You can enter.");
-} else if ( hastiket === "yes" && lt === "yes") {
-    alert("You cant enter.");
-} 
-=
+const numbers=[12, 6, 92, 64,];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        console.log("luwi number:", numbers[i]);
+    } else {
+        console.log("kenti number:", numbers[i]);
+    }
+}
